7a44485fe50bbf4f09400e0bd8655f39f4c29cdd
Ankita6039 <ankitathakur6039@gmail.com>
2023-04-24 16:06:47 +0530

    added footer and para

--- components/slider.jsx
+++ components/slider.jsx
@@ -43,7 +43,7 @@ function Slider() {
   };
 
   return (
-    <div className='max-w-[1600px] h-[600px] w-full m-auto relative group '>
+    <div className='max-w-[1600px] h-[600px] w-full m-auto relative group border-b-4 border-orange-600'>
       <div
         style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
         className='w-full h-full bg-center bg-cover duration-500'
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,11 +8,11 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      }, colors: {
+        'hero': "url('D:\majorproject\ideal-adventure\public\desert.jpg')",
+      }, 
+      colors: {
         'custom-yellow' : '#fefee4',
+        'footer-color' : '#f4f4d3',
       },
     },
   },
